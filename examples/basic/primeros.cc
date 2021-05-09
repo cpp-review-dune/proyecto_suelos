ad8e3687f4dc6394775d178661e47ce5ab716846
John Leal <jlealgom@unal.edu.co>
2021-05-09 16:17:41 -0400

    Desarrollando el manual

--- examples/basic/primeros.cc
+++ examples/basic/primeros.cc
@@ -12,5 +12,12 @@ using Mat = DenseMatrix<Number>;
 
 int main(){
     hdnum::Vector<float> x(10);
-    std::cout<< x << std::endl;
+    std::cout<<"Se definió el vector x con 10 elementos"<< x << std::endl;
+    std::cout<<"El comando utilizado fue hdnum::Vector<float> x(10)"<<std::endl;
+    std::cout<<"También se puede definir como hdnum::Vector<double> y(10,3.14) 10 elementos del número pi"<<std::endl;
+    hdnum::Vector<double> y(10,3.14);
+    std::cout<<"y ="<<y<<std::endl;
+    std::cout<<"hdnum::Vector<float> a, genera como resultado: "<<std::endl;
+    hdnum::Vector<float> a;
+    std::cout<<a<<std::endl;
 }
