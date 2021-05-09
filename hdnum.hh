5dce271bc5032f3e5ebc21c68937ab7c8c611725
asoleal <63559276+asoleal@users.noreply.github.com>
2021-05-09 09:50:24 -0500

    Initialize lehramt

---
+++ hdnum.hh
@@ -0,0 +1,29 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifndef HDNUM_HDNUM_HH
+#define HDNUM_HDNUM_HH
+
+#define HDNUM_DEVEL_MODE 1
+
+#define HDNUM_HAS_GMP 0
+
+#include <math.h>
+#include <complex>
+#if HDNUM_HAS_GMP
+#include <gmpxx.h>
+#endif
+
+#include "src/exceptions.hh"
+#include "src/vector.hh"
+#include "src/densematrix.hh"
+#include "src/timer.hh"
+#include "src/opcounter.hh"
+#include "src/precision.hh"
+#include "src/lr.hh"
+#include "src/ode.hh"
+#include "src/pde.hh"
+#include "src/sgrid.hh"
+#include "src/newton.hh"
+#include "src/rungekutta.hh"
+
+
+#endif
