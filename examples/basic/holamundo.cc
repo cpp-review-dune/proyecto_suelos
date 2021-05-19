c4d4368579d40254bdee322d84c99dac0a2196db
asoleal <jlealgom@unal.edu.co>
2021-05-19 14:04:54 +0000

    Creación carpeta Makefile y prueba con make, creación de un archivo .sh y ejecución

--- examples/basic/holamundo.cc
+++ examples/basic/holamundo.cc
@@ -1,5 +1,9 @@
 #include<iostream>
 int main(){
     std::cout<<"Hola mundo\n";
+    char a[10];
+    std::cout<<"Digite su nombre "<<std::endl;
+    std::cin>>a;
+    std::cout<<"Hola "<<a<<std::endl;
     return 0;
 }
