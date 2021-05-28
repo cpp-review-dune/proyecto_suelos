cf444a744efc59adab1f4e582826b75f1a57ffc3
Juliette009 <krojasl@unal.edu.co>
2021-05-28 20:41:37 +0000

    Creacion de archivo euler.cc

--- examples/juliette/ejecuciónjul.sh
+++ examples/juliette/ejecuciónjul.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-clear
-ls
-g++ hola.cc -o primero 
-ls
-./primero
+#clear
+#ls
+#g++ hola.cc -o primero 
+#ls
+#./primero
+make
+./prueba02
--- examples/juliette/hola.cc
+++ examples/juliette/hola.cc
@@ -3,7 +3,7 @@
 int main() {
     char a[20];
 
-    std::cout<<"Hola mundo"<<std::endl;
+    std::cout<<"Hola mundo "<<std::endl;
     std::cout<<"Hola, digite su nombre"<<std::endl;
     std::cin>>a;
     std::cout<<"Hola," <<a<<std::endl;
