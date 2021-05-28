73d428ab08993d912ea94b35c1bdcd020b950a97
Juliette009 <krojasl@unal.edu.co>
2021-05-28 19:55:55 +0000

    Creacion de archivo ejecutable en shell

--- examples/juliette/hola.cc
+++ examples/juliette/hola.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+    char a[20];
+
     std::cout<<"Hola mundo"<<std::endl;
+    std::cout<<"Hola, digite su nombre"<<std::endl;
+    std::cin>>a;
+    std::cout<<"Hola," <<a<<std::endl;
     return 0;
 }
