120dcdb9a8f03859b3e65c974191b7ac4eab4427
debiansita <debiansita@debiansita.debiansita>
2021-06-23 12:47:12 -0500

    Actualizacion

---
+++ examples/Guille/programacion/condicional.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+    int x=3;
+
+    if (x>0){
+        std::cout <<"x es positivo"<<std::endl;
+    }
+
+    else if(x<0){
+        std::cout <<"x es negativo"<< std::endl;
+    }
+
+    else{
+        std::cout <<"x es cero"<<std::endl;
+    }
+
+
+
+
+    return 0;
+}
