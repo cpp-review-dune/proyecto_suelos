0b45f36277a3e6909cad8a578688dcb64a556e3f
john <jlealgom@unal.edu.co>
2021-07-19 16:32:14 -0500

    Función booleana

---
+++ examples/basic/bool.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main(){
+int x;
+//prompt the user for input;
+std::cout<<"Enter the integer\n";
+
+//get input
+std::cin>>x;
+
+// check and see if the input statement succeeded
+  if (std::cin.good() == false) {
+    std::cout << "That was not an integer." << std::endl;
+    return -1;
+  }
+
+// print the value we got from the user
+  std::cout << x << std::endl;
+  return 0;
+}
