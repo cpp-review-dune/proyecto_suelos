7e007c8f4f7c981e1a4177da739d624e579c4b57
debiansita <debiansita@debiansita.debiansita>
2021-07-21 11:52:57 -0500

    Up time Guille

---
+++ examples/Guille/programacion/time.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+struct Time {
+  int hour, minute;
+  double second;
+};
+
+
+int main(){
+
+    Time time{11, 59, 3.14159};
+
+    std::cout<<time.hour<<" hour "<<time.minute<<" minute "<<time.second<<" second "<<std::endl;
+
+    return 0;
+}
