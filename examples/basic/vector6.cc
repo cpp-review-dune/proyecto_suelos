36427870c606e66491ab573d5e53c5a7bcd5e512
john <jlealgom@unal.edu.co>
2021-07-24 09:13:32 -0500

    update vector6

--- examples/basic/vector6.cc
+++ examples/basic/vector6.cc
@@ -2,7 +2,7 @@
 #include <vector>
 int main(){
     std::vector<int> count(4,0);
-    std::vector<double> doubleVector;
+    std::vector<double> doublevector;
     count[0]=8;
     count[1]=count[0]*2;
     count[2]++;
@@ -12,5 +12,22 @@ int main(){
         std::cout << count[i] << std::endl;
         i++;
     }
+    //otra forma de imprimir los vectores
+    for (int i = 0; i < 4; i++) {
+        std::cout << count[i] << std::endl;
+    }
+    //dejando que lea hasta el final del vector
+    int j;
+    for (auto j=count.begin();j!=count.end();j++)
+        std::cout<<*j<<"  ";
+    std::cout<<std::endl;
+    std::vector<int> g1;
+    //va colocando el valor al final, y aumenta el índice
+    for (int i = 1; i <= 5; i++)
+        g1.push_back(i);
+  
+    std::cout << "Output of begin and end: ";
+    for (auto i = g1.begin(); i != g1.end(); ++i)
+        std::cout << *i << " ";
     return 0;
 }
