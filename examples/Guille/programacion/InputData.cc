3f4a2306e0183b86b5d1c5276219d0e2aa3d9567
debian-jjlg <debian-jjlg@debian-jjlg.debian-jjlg>
2021-08-04 11:24:33 -0500

    update

--- examples/Guille/programacion/InputData.cc
+++ examples/Guille/programacion/InputData.cc
@@ -6,30 +6,30 @@ double promX(double *x,int n);
 
 int main()
 {
-
-
 double *x, prom;
 FILE *archDat, *archRes;
 int n, i;
-archDat = fopen("ldatos1", "r");
+archDat = fopen("ldatos1.txt", "r");
 if( archDat == NULL ){
-
-}
 printf("\n\n Archivo inexistente.\n\n");
 exit (1) ;
-fscanf (archDat, "%d", &n);
+}
+fscanf(archDat,"%d",&n);
+std::cout<<"n = "<<n<<std::endl;
 x = new double[n];
 if ( x == NULL ){
 printf("\n\n Memoria insuficiente.\n\n");
 exit(1);
 }
 for( i=0; i<n; i++) {
-    fscanf(archDat, "%lf", &x[i]);
+    fscanf(archDat,"%lf",&x[i]);
+    std::cout<< *(x+i) <<std::endl;
     }
 fclose (archDat);
-archRes = fopen("ejemplo.res", "W");
-prom = promX(x, n);
-fprintf (archRes, " promedio %12. 4lf\n ", prom);
+archRes = fopen("ejemplo.res", "w");
+prom = promX(x,n);
+printf("Promedio %12.4lf\n",prom);
+fprintf (archRes,"Promedio %12.4lf\n ", prom);
 fclose(archRes);
 return 0;
 }
