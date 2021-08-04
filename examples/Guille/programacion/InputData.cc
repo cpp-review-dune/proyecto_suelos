efa2dfb04d345eaa1364aa432618a0884dd0b26c
john <jlealgom@unal.edu.co>
2021-08-04 09:49:40 -0500

    Read data file

---
+++ examples/Guille/programacion/InputData.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+
+double promX(double *x,int n);
+
+int main()
+{
+
+
+double *x, prom;
+FILE *archDat, *archRes;
+int n, i;
+archDat = fopen("ldatos1", "r");
+if( archDat == NULL ){
+
+}
+printf("\n\n Archivo inexistente.\n\n");
+exit (1) ;
+fscanf (archDat, "%d", &n);
+x = new double[n];
+if ( x == NULL ){
+printf("\n\n Memoria insuficiente.\n\n");
+exit(1);
+}
+for( i=0; i<n; i++) {
+    fscanf(archDat, "%lf", &x[i]);
+    }
+fclose (archDat);
+archRes = fopen("ejemplo.res", "W");
+prom = promX(x, n);
+fprintf (archRes, " promedio %12. 4lf\n ", prom);
+fclose(archRes);
+return 0;
+}
+
+double promX(double *x,int n){
+    int i;
+    double suma = 0.0;
+    for(i = 0;i<n;i++){
+        suma += *(x+i);
+    }
+    return suma/n;
+}
