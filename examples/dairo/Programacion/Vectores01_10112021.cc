438ce6595ad089144ce42ec4dcc5b58d58b14eb8
dairopena <dairocardenas937@gmail.com>
2021-10-11 16:54:41 +0000

    Manejo de la libreria HDNUM

---
+++ examples/dairo/Programacion/Vectores01_10112021.cc
@@ -0,0 +1,15 @@
+#include <iostream>    // notwendig zur Ausgabe
+#include "hdnum.hh"    // hdnum header
+
+int main ()
+{
+    hdnum :: Vector < float > x (10);// Vector con 10 elementos
+    hdnum :: Vector < double > y (10 ,3.14);// Vector con 10 elementos inicializado
+    hdnum :: Vector < float > a ;// Vector sin elementos
+    for (std::size_t i =0; i < x.size(); i = i +1) // Acceso a cada elemento
+        x[i] = i;
+    std::cout<<"El vector x es: "<<std::endl;
+    std::cout << x <<std::endl; 
+    
+return 0;
+}
