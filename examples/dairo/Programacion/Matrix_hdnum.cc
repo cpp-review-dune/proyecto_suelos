f6214272d74999c8bc6f3668fb7c9928d572b40b
dairopena <dairocardenas937@gmail.com>
2021-12-01 19:11:12 +0000

    Matrix Hdnum

---
+++ examples/dairo/Programacion/Matrix_hdnum.cc
@@ -0,0 +1,18 @@
+#include <iostream>    // notwendig zur Ausgabe
+#include "hdnum.hh"    // hdnum header
+
+int main(){
+    hdnum :: DenseMatrix <float> B (10 ,10); 
+    hdnum :: DenseMatrix <float> C (10 ,10 ,2.33); 
+    
+    for(int i =0; i <B. rowsize (); ++ i )
+        for(int j =0; j <B. colsize (); ++ j )
+            B[i ][ j] = 1.33;
+     
+
+std::cout<<"Matriz B :"<< B <<std::endl;
+std::cout<<"Matriz C :"<< C <<std::endl;
+
+return 0;
+
+}
